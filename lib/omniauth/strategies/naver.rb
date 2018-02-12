6341be0f897a53759ead519fa2a877565ad9a46e
byung82 <byung82@gmail.com>
2018-02-13 03:32:07 +0900

    profile_image include? nil 일경우

--- lib/omniauth/strategies/naver.rb
+++ lib/omniauth/strategies/naver.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
+    # Naver OAuth2
     class Naver < OmniAuth::Strategies::OAuth2
       option :name, 'naver'
 
-      option :client_options, {
-        :site => 'https://nid.naver.com',
-        :authorize_url => 'https://nid.naver.com/oauth2.0/authorize',
-        :token_url => 'https://nid.naver.com/oauth2.0/token',
-      }
+      option :client_options, site: 'https://nid.naver.com',
+                              authorize_url: 'https://nid.naver.com/oauth2.0/authorize',
+                              token_url: 'https://nid.naver.com/oauth2.0/token'
 
       uid { raw_properties['enc_id'].to_s }
 
@@ -18,12 +19,12 @@ module OmniAuth
           'name' => raw_properties['name'],
           'email' => raw_properties['email'],
           'gender' => gender,
-          'image' => image,
+          'image' => image
         }
       end
 
       extra do
-        {:raw_info => raw_info}
+        { raw_info: raw_info }
       end
 
       private
@@ -34,7 +35,8 @@ module OmniAuth
       end
 
       def image
-        return raw_properties['profile_image'].sub('?type=s80', '') unless raw_properties['profile_image'].include? 'nodata_33x33.gif'
+        return raw_properties['profile_image'].sub('?type=s80', '') unless
+          raw_properties['profile_image'].try(:include?, 'nodata_33x33.gif')
       end
 
       def raw_info
